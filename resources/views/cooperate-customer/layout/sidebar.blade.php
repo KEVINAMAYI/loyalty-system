4c22b83dc961f759925f1fe76ffd54fd3238cfc6
KEVINAMAYI <kevinamayi20@gmail.com>
2022-03-25 05:45:16 -0700

    intial commit

---
+++ resources/views/cooperate-customer/layout/sidebar.blade.php
@@ -0,0 +1,44 @@
+<aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3 " id="sidenav-main">
+    <div class="sidenav-header">
+      <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
+      <a class="navbar-brand m-0" href="https://demos.creative-tim.com/soft-ui-dashboard/pages/dashboard.html" target="_blank">
+        <span style="margin-right:5px;" class="login100-form-logo">
+          <img src="assets/img/logo.jpg" style="border-radius:100%;" width="70" height="70">
+        </span>
+        <span class="ms-1 font-weight-bold">OLA</span>      
+      </a>
+    </div>
+    <hr class="horizontal dark mt-0">
+    <div class="collapse navbar-collapse  w-auto  max-height-vh-100 h-100" id="sidenav-collapse-main">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="/dashboard">
+            <i class="fa-solid fa-house"></i>
+            <span class="nav-link-text ms-1">Dashboard</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link  " href="/employees">
+            <i class="fa-solid fa-person"></i>
+            <span class="nav-link-text ms-1">Employees</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link  " href="/vehicles">
+            <i class="fa-solid fa-car"></i>
+            <span class="nav-link-text ms-1">Vehicle</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link  " href="rewards.html">
+            <i class="fa-solid fa-lock-open"></i>
+            <span class="nav-link-text ms-1">Authorize Fuel Purchase</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="index.html" class="nav-link  " href="#">
+            <i class="fa-solid fa-arrow-right-from-bracket"></i>
+            <span class="nav-link-text ms-1">Logout</span>
+          </a>
+        </li>
+  </aside>
