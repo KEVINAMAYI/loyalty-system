8c246b8d9716e8cbb250d9906a2c78d2e64bc737
KEVINAMAYI <kevinamayi20@gmail.com>
2022-05-03 23:10:53 -0700

    sorted an error on home page for access

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,10 +1,20 @@
 <?php
 
 namespace App\Http\Controllers;
-use Illuminate\Support\Facades\Auth;
 
 
+use Config;
+use Exception;
+use App\Models\Sale;
+use App\Models\User;
+use App\Models\Vehicle;
+use Twilio\Rest\Client;
+use App\Models\Customer;
 use Illuminate\Http\Request;
+use App\Models\AuthorizedPurchase;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class HomeController extends Controller
 {
@@ -31,7 +41,34 @@ class HomeController extends Controller
 
             if($role == "Corperate")
             {
-                return view("cooperate-customer.dashboard");
+                $user = Auth::user()->name;
+                $employees = Customer::where('type','=',$user)->get();
+                $vehicles =  Vehicle::where('ownership','=', $user)->get();
+                $autorizedpurchases = AuthorizedPurchase::where('name','=', $user)->get();
+                $employees_authorized_data = array();
+                $company = User::where('id','=',$user)->get();
+        
+        
+                foreach ($autorizedpurchases as $autorizedpurchase)
+                {
+                    $customerid = $autorizedpurchase->employee_id;
+                    $vehicleid = $autorizedpurchase->vehicle_id;
+                    $personal_data = Customer::where('id','=',$customerid)->get();
+                    $vehicle_data = Vehicle::where('id','=',$vehicleid)->get();
+        
+        
+                    $employee = array();
+        
+                    array_push($employee, $personal_data);
+                    array_push($employee, $vehicle_data);
+                    array_push($employee,  $autorizedpurchase);
+                    array_push($employees_authorized_data,$employee);
+        
+                }
+        
+                 return view('cooperate-customer.dashboard')->with(['employees' => $employees,'vehicles' => $vehicles,'authorized_purchases' => $employees_authorized_data, 'company' => $company]);
+               
+               
     
             }
             else
